965ffba3a120d6960179eab5fbf956eb0e801060
pomiska <miska99@live.fi>
2022-11-25 04:20:48 +0200

    tehtävä 5 step 8

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -5,20 +5,20 @@ class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
         self.yhteishinta = 0
-        self.ostokset = []
+        self.ostokset_list = []
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
-        return len(self.ostokset)
+        return len(self.ostokset_list)
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
         return self.yhteishinta
 
     def lisaa_tuote(self, lisattava: Tuote):
-        self.ostokset.append(Ostos(lisattava))
+        self.ostokset_list.append(Ostos(lisattava))
         self.yhteishinta += lisattava.hinta()
 
     def poista_tuote(self, poistettava: Tuote):
@@ -30,6 +30,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        return self.ostokset_list
